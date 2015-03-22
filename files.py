0854753733198015f5a9c4e14e2b57745476d2a9
Robert Hodgen <roberthodgen@opphub.net>
2015-03-22 07:11:21 -0400

    File serving works

--- files.py
+++ files.py
@@ -2,12 +2,15 @@ from google.appengine.ext import blobstore
 
 from google.appengine.ext.webapp import blobstore_handlers
 
+from google.appengine.api.images import get_serving_url
+
 import webapp2
 
 import json
 
 from google.appengine.api import urlfetch
 
+import urllib
 
 
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
@@ -23,7 +26,6 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
         upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
         blob_info = upload_files[0]
         # Hook to update Parse database...
-        print self.request.POST.get('username')
         payload = {
                 'username': self.request.POST.get('username'),
                 'resume_url': ''.join(['https://handshake-hack.appspot.com/files/', str(blob_info.key())])
@@ -35,14 +37,22 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
                 'X-Parse-Application-Id': 'Aw2wgeVa9ihzU3OqRggicJ2rcFoQFxQYIWDhnaFR',
                 'X-Parse-REST-API-Key': 'TWEMo9AIIRXCUCfesvRI49Q4mqPM7Zw5muXW239G'
             })
-        # print json.dumps(result)
-        print result.content
-        print result.headers
-        
-        if result.status_code == 200:
+        if result.status_code >= 200 and result.status_code <= 299:
             self.redirect('/success.html')
         else:
             self.redirect('/error')
+
+
+class DownloadHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, fileId=None):
+        """ Return a link to download a file. """
+
+        if not fileId:
+            abort(400)
+
+        blob_info = blobstore.BlobInfo.get(fileId)
+        # self.send_blob(blob_info)  # For some reason this wasn't working
+        self.redirect(get_serving_url(blob_info.key()))  # Isntead we'll redirect to a serving link
 
 
 app = webapp2.WSGIApplication([
@@ -50,5 +60,9 @@ app = webapp2.WSGIApplication([
         '/upload',
         handler=UploadHandler,
         methods=['GET', 'POST']
+    ), webapp2.Route(
+        '/files/<fileId>',
+        handler=DownloadHandler,
+        methods=['GET']
     )
 ])
